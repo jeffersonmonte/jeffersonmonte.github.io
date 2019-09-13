0aa15bcfde3651b8f2a7aea9b7d335b6f655ca0d
Jefferson Monteiro <jefferson@webmais.net.br>
2019-09-12 23:29:48 -0300

    incluindo npcs

--- js/canvas-mapa.js
+++ js/canvas-mapa.js
@@ -3,7 +3,6 @@ var canvasMapa = document.getElementById("mapa");
 var canvasWidth = document.getElementById("painel-mapa").clientWidth;
 var canvasHeight = document.getElementById("painel-mapa").clientHeight;
 
-console.log(canvasWidth +" - "+ canvasHeight);
 
 canvasMapa.width = canvasWidth
 canvasMapa.height = canvasHeight;
@@ -11,7 +10,27 @@ canvasMapa.style.background = "rgba(0,0,0,0.1)";
 var ctxMapa = canvasMapa.getContext("2d");
 
 const img = new Image();
-        
+
+var fileTag = document.getElementById("myMap");
+fileTag.addEventListener("change", function() {
+    changeImage(this);
+});
+
+function changeImage(input) {
+var reader;
+
+if (input.files && input.files[0]) {
+    reader = new FileReader();
+
+    reader.onload = function(e) {
+    //preview.setAttribute('src', e.target.result);
+    carregaImagem(e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+}
+}  
+
 carregaImagem();
 
 function carregaImagem(imagem)
--- js/tokens.js
+++ js/tokens.js
@@ -1,4 +1,18 @@
-$(document).ready(function(){
+function criarNPC()
+{
+    var nomeNPC = document.getElementById('nomeNPC').value;
+    var tamanhoNPC = document.getElementById('tamanhoNPC').value;
+    var qtdNPC = document.getElementById('qtdNPC').value;
+    
+    for(var i = 0; i<qtdNPC; i++)
+    {
+        var npc = new NPC(tamanhoNPC, "img/dagao.jpg", nomeNPC);  
+    }
+};
+
+var NPC = function(tamanho, imagem, nome)
+{
+    this.tamanho = tamanho;
     var mousePosition;
     var offset = [0,0];
     var div;
@@ -8,9 +22,30 @@ $(document).ready(function(){
     div.style.position = "absolute";
     div.style.left = "0px";
     div.style.top = "80px";
-    div.style.width = "100px";
-    div.style.height = "100px";
-    div.style.backgroundImage = "url('img/dagao.jpg')";
+    if(tamanho == "medio")
+    {
+        div.style.width = "50px";
+        div.style.height = "50px";
+        div.style.backgroundSize = "50px 50px";
+    }else if(tamanho == "grande")
+    {
+        div.style.width = "100px";
+        div.style.height = "100px";
+        div.style.backgroundSize = "100px 100px";
+    }else if(tamanho = "gigante")
+    {
+        div.style.width = "150px";
+        div.style.height = "150px";
+        div.style.backgroundSize = "150px 150px";
+    }else if(tamanho = "colossal")
+    {
+        div.style.width = "200px";
+        div.style.height = "200px";
+        div.style.backgroundSize = "200px 200px";
+    }
+    
+    div.style.backgroundImage = "url('"+imagem+"')";
+    div.style.backgroundSize = ""+div.style.width+"px"+ div.style.height+"px";
     div.style.color = "blue";
 
     document.querySelector("#painel-mapa").appendChild(div);
@@ -22,11 +57,11 @@ $(document).ready(function(){
             div.offsetTop - e.clientY
         ];
     }, true);
-
+    
     document.addEventListener('mouseup', function() {
         isDown = false;
     }, true);
-
+    
     document.addEventListener('mousemove', function(event) {
         event.preventDefault();
         if (isDown) {
@@ -41,4 +76,4 @@ $(document).ready(function(){
         }
     }, true);
 
-})
+}
